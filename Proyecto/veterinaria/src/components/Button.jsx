9b40c952fc2adda0991fa0560206f2d02016f288
liss001 <liss987barrientos@gmail.com>
2025-06-01 14:41:57 -0400

    Delete Proyecto/veterinaria directory

--- Proyecto/veterinaria/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Button({ label, onClick, styleType = "primary" }) {
-  const className = `btn btn-${styleType}`;
-  return <button className={className} onClick={onClick}>{label}</button>;
-}
-
-export default Button;
