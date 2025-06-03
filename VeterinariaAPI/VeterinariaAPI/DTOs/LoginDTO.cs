bcfbd6bb617c4034292d71aea95216ea3d4ac881
liss001 <liss987barrientos@gmail.com>
2025-06-03 09:10:37 -0400

    Delete VeterinariaAPI directory

--- VeterinariaAPI/VeterinariaAPI/DTOs/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿// DTOs/LoginDTO.cs
-namespace VeterinariaAPI.DTOs
-{
-    public class LoginDTO
-    {
-        public string NombreUsuario { get; set; } = null!;
-        public string Contraseña { get; set; } = null!;
-    }
-}
--- VeterinariaAPI/VeterinariaAPI/DTOs/UsuarioDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VeterinariaAPI.DTOs
-{
-    public class UsuarioDTO
-    {
-        public int Id { get; set; }
-        public string NombreUsuario { get; set; } = null!;
-        public string Rol { get; set; } = null!;
-    }
-}
